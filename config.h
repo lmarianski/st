23535a358f179a9f072d0170417d334ac371fa53
Łukasz Mariański <lmarianski@protonmail.com>
2021-07-18 18:18:31 +0200

    colorscheme stuff

--- config.h
+++ config.h
@@ -121,20 +121,24 @@ static const char *colorname[] = {
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"black", /* background */
-	"white", /* foreground */
-	"white"  /* cursorColor */
+	"#cccccc",
+	"#555555",
+	"black"
 };
 
 
 /*
  * Default colors (colorname index)
- * foreground, background, cursor, reverse cursor
+ * background, foreground, cursor, reverse cursor
  */
-unsigned int defaultfg = 257;
-unsigned int defaultbg = 256;
-static unsigned int defaultcs = 258;
-static unsigned int defaultrcs = 258;
+// unsigned int defaultbg = 256;
+// unsigned int defaultfg = 257;
+// static unsigned int defaultcs = 258;
+// static unsigned int defaultrcs = 258;
+unsigned int defaultbg = 0;
+unsigned int defaultfg = 7;
+static unsigned int defaultcs = 256;
+static unsigned int defaultrcs = 257;
 
 /*
  * Default shape of cursor
